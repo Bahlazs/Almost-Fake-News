d7350573d6713f3e172fd6a4c571232641ee0936
bahlazs <nick.balazs18@gmail.com>
2023-08-17 17:23:42 +0200

    add main article and news card styling

--- afnFrontend/src/components/NewsCard.jsx
+++ afnFrontend/src/components/NewsCard.jsx
@@ -3,7 +3,9 @@ import './stylesheets/NewsCard.css'
 const NewsCard = ({article}) => {
     return (
         <div className="news-card">
-            <div></div><h3 className="news-title">{article.title}</h3>
+            <div className={"nt-container"}>
+                <h3 className="news-title">{article.title}</h3>
+            </div>
             <h4 className="news-head">{article.newsHeader}</h4>
             <p className="news-date">{article.newsDate}</p>
         </div>
