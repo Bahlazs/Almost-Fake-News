61cb4a17176efc879a4c06d718e4b9c41b8c1f95
Nick Balázs <99294505+Bahlazs@users.noreply.github.com>
2023-08-22 07:06:07 +0200

    add smooth scrolling

--- afnFrontend/src/components/NavBar.jsx
+++ afnFrontend/src/components/NavBar.jsx
@@ -15,20 +15,20 @@ const NavBar = () => {
     return (
         <div className="navbar-container">
             <nav className="navbar">
-                <Link to="message" ><h2 className="page-title" >Almost Fake News</h2></Link>
+                <Link  to="message" spy={true} smooth={true} duration={500}><h2 className="page-title" >Almost Fake News</h2></Link>
                 <div className='hamburger-icon' onClick={handleClick}>
                     {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
                         : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
                 </div>
                 <ul className={click ? "navbar-menu active" : "navbar-menu"}>
                     <li className="navbar-item">
-                        <Link to="message" onClick={closeMenu}>Home</Link>
+                        <Link to="message" onClick={closeMenu} spy={true} smooth={true} duration={500}>Home</Link>
                     </li>
                     <li className="navbar-item">
-                        <Link to="news-grid" onClick={closeMenu}>News</Link>
+                        <Link to="news-grid" onClick={closeMenu} spy={true} smooth={true} offset={50} duration={500}>News</Link>
                     </li>
                     <li className="navbar-item">
-                        <Link to="contacts" onClick={closeMenu}>Contacts</Link>
+                        <Link to="contacts" onClick={closeMenu} spy={true} smooth={true} offset={50} duration={500}>Contacts</Link>
                     </li>
                 </ul>
             </nav>
